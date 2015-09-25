e63b42904a39d287772cae6b04e0f9f0101e7e4f
Sean Patrick Floyd <Sean.Floyd@zalando.de>
2015-09-25 11:44:47 +0200

    added package-info

---
+++ java/src/main/java/org/zalando/guild/api/json/fields/java/util/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * We're copying a lot of existing functionality here. Perhaps we should replace this package with a dependency to Guava
+ * (or a shaded version thereof).
+ *
+ * @author  Sean Patrick Floyd (sean.floyd@zalando.de)
+ * @since   25.09.2015
+ */
+package org.zalando.guild.api.json.fields.java.util;
